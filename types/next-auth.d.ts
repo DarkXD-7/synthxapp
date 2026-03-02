cff973e41cf6dfeaa2e6e0a9f76f9d3a6220d1ec
Dark <suhansalian740@gmail.com>
2026-03-02 15:26:32 +0530

    Update next-auth.d.ts

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -4,6 +4,12 @@ import "next-auth/jwt";
 declare module "next-auth" {
   interface Session {
     accessToken?: string;
+    user?: {
+      id?: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    };
   }
 }
 
@@ -11,5 +17,6 @@ declare module "next-auth/jwt" {
   interface JWT {
     accessToken?: string;
     tokenType?: string;
+    sub?: string; // Discord user ID lives here
   }
 }
